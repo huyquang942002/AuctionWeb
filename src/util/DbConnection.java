8e8c02b1dd4ef6e969dc7f1a28b0b55cd0482ca3
NongQuangHuy <20110496@student.hcmute.edu.vn>
2022-12-23 01:14:15 +0700

    update database

--- src/util/DbConnection.java
+++ src/util/DbConnection.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
  
 public class DbConnection {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/ecom?useSSL=false";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "P@ssw0rd";
+    private static final String URL = "jdbc:mysql://us-cdbr-east-06.cleardb.net/heroku_2a4df7540abeb45?enabledTLSProtocols=TLSv1.2";
+    private static final String USERNAME = "b157cde6369d2f";
+    private static final String PASSWORD = "8bb7a353";
+
     public Connection connection = null;
     
     public DbConnection() {
